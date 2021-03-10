27b7c50da6430a7e4bfbd89db846d51ecfaf38dd
Azur Ramic <azurramic@gmail.com>
2021-03-10 11:12:06 +0100

    Add comments in index.js

---
+++ ex02/index.js
@@ -0,0 +1,14 @@
+function main () {
+    // Just another line of comment
+
+    /* Just another line of comment with more than 5 letter - row 1 
+    Just another line of comment with more than 5 letter - row 2
+    Just another line of comment with more than 5 letter - row 3
+    Just another line of comment with more than 5 letter - row 4
+    */
+
+}
+
+console.log(main());
+
+module.exports = main;
