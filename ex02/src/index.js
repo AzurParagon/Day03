b1838ae17a02250b3d5566f34ffa221ba59f93fe
Azur Ramic <azurramic@gmail.com>
2021-03-11 10:24:43 +0100

    update index.js

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,4 +1,5 @@
 function main () {
+    // Only change code below this line
     // Just another line of comment
 
     /* Just another line of comment with more than 5 letter - row 1 
@@ -6,7 +7,7 @@ function main () {
     Just another line of comment with more than 5 letter - row 3
     Just another line of comment with more than 5 letter - row 4
     */
-
+    // Only change code above this line 
 }
 
 console.log(main());
